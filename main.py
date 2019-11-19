431830e92765a115a0ba0dd44807826b13df68bb
Sean Liu <119928@kaneland.org>
2019-11-19 19:47:12 +0000

    sus mann

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
 print("Welcome to Regret,\n\na happy little innocent text based semi-procedurally generated wizardry dungeon crawler RPG.\n")
+fails = 0
+
 while True:
-  difficulty = input("How much do you regret? Low or high?\n").strip().lower()
-  print("")
-  if difficulty[0][0] == "l":
-    print("Low. It's a simple start.\n\n")
-  elif difficulty[0][0] == "h":
-    print('sus box')
+  difficulty = input("How much do you regret? Low or enough?\n").strip().lower()
+  print()
+  if len(difficulty) != 0:
+    if difficulty[0][0] == "l":
+      print("Low? It's a simple start.\n\n")
+    elif difficulty[0][0] == "e":
+      print("Enough? That's going to hurt, however much that is.\n\n")
+    else:
+      print("Thats not what I asked for.")
+      fails += 1
   else:
-    print("thats not what I asked for")
+    print("Thats not what I asked for.")
+    fails += 1
   #if letter == :
   #  print("There is a P!")
   # break #If a p is found, exit the loop

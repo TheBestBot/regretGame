e0249109d9938144433b39770d5f6c0ea0250c2a
Sean Liu <119928@kaneland.org>
2019-11-19 19:53:50 +0000

    pice by pice fun

--- main.py
+++ main.py
@@ -1,23 +1,33 @@
 print("Welcome to Regret,\n\na happy little innocent text based semi-procedurally generated wizardry dungeon crawler RPG.\n")
 fails = 0
+import sys, time
+
+def Piece(text, speed):
+  for c in text:
+      sys.stdout.write(c)
+      sys.stdout.flush()
+      time.sleep(0.05)
+  print
+
 
 while True:
-  difficulty = input("How much do you regret? Low or enough?\n").strip().lower()
+  Piece("How much do you regret? Low or enough?\n")
+  difficulty = input().strip().lower()
   print()
   if len(difficulty) != 0:
     if difficulty[0][0] == "l":
+      time.sleep(0.5)
       print("Low? It's a simple start.\n\n")
+      break
     elif difficulty[0][0] == "e":
+      time.sleep(0.5)
       print("Enough? That's going to hurt, however much that is.\n\n")
+      break
     else:
+      time.sleep(0.5)
       print("Thats not what I asked for.")
       fails += 1
   else:
+    time.sleep(0.5)
     print("Thats not what I asked for.")
     fails += 1
-  #if letter == :
-  #  print("There is a P!")
-  # break #If a p is found, exit the loop
-
-#else: #Otherwise if no P, tell user
-#  print("There is no letter P in the phrase")
